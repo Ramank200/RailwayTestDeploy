b9e12792b271ead6b26b5faa5b415c4286ca0470
Ramanakablah <alexesramon0909@gmail.com>
2025-03-08 12:33:53 +0530

    Push/ Envs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
+require("dotenv").config();
 const app = express();
-
+const PORT = process.env.PORT || 8080;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.listen(8800, () => {
-  console.log("Example app listening on port 8800!");
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
